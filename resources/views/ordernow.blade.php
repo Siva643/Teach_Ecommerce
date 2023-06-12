c2b10501fcb8f14a384758c3fdb6ed7733337546
Siva <jmsiva123@gmail.com>
2023-06-12 17:28:38 -0400

    some_include

--- resources/views/ordernow.blade.php
+++ resources/views/ordernow.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+ {{View::make('link')}}
   <style>
     .border{
      border:1px solid gray;
@@ -20,19 +21,19 @@
    <table class="table">
       <tr>
         <td class="fw-bold">Amount</td>
-        <td>$ {{$total}}</td>
+        <td>{{$total}} INR</td>
       </tr>
       <tr>
         <td class="fw-bold">Tax</td>
-        <td>$ 0</td>
+        <td>0 INR</td>
       </tr>
       <tr>
         <td class="fw-bold">Delivery</td>
-        <td>$ 10</td>
+        <td>750 INR</td>
       </tr>
       <tr>
         <td class="fw-bold">Total Amount</td>
-        <td>$ {{$total+10}}</td>
+        <td class="fw-bold text-success">{{$total+750}} INR</td>
       </tr>
       </table>
     <form action="/orderplace" method="post">
